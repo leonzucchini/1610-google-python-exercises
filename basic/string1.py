fd437a183ef81b7ab1519dd79910e315749a7413
leonzucchini <leon.zucchini@gmail.com>
2016-10-09 15:09:55 +0200

    Exercise D
    Done and checked

--- basic/string1.py
+++ basic/string1.py
@@ -67,8 +67,8 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  ans = b[:2] + a[2:] + ' ' + a[:2] + b[2:]
+  return ans
 
 
 # Provided simple test() function used in main() to print
