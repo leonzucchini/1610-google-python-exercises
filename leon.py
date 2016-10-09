d49beee96ff10697dbcac23aa3d9e76b5b79e3b2
leonzucchini <leon.zucchini@gmail.com>
2016-10-09 14:58:25 +0200

    Initial commit
    Exercise A strings 1 already completed

---
+++ leon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python -tt
+
+import sys
+
+def Hello(name):
+    if name == 'Alice' or name == 'Nick':
+        name = name + '???'
+    else:
+        DoesNotExist(name)
+    name = name + '!!!!'
+    print 'Hello', name
+
+def main():
+	Hello(sys.argv[1])
+
+if __name__ == '__main__':
+  main()
