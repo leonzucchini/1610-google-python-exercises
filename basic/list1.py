2147ce86856a44a44d3960a4d5759edf639ea601
leonzucchini <leon.zucchini@gmail.com>
2016-10-09 19:08:46 +0200

    list exercises 1
    need to remember to fork before editing

--- basic/list1.py
+++ basic/list1.py
@@ -21,8 +21,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  count = 0
+  for i in words:
+  	if len(i) > 1 and i[0] == i[-1]:
+  		count += 1
+  return count
 
 
 # B. front_x
@@ -33,8 +36,17 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
-  return
+  xlist = []
+  ylist = []
+  
+  for i in words:
+  	if i[0] == 'x':
+  		xlist.append(i)
+  	else:
+  		ylist.append(i)
+  
+  ans = sorted(xlist) + sorted(ylist)
+  return ans
 
 
 
@@ -45,8 +57,12 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-  # +++your code here+++
-  return
+  
+  def lastValue(tuple):
+  	return tuple[-1]
+  
+  ans = sorted(tuples, key = lastValue)
+  return ans 
 
 
 # Simple provided test() function used in main() to print
